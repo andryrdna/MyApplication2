8ca024d92c2910f36b1609024da1bb3dd8809c81
andryrdna <sim.andry1@gmail.com>
2019-01-24 15:30:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/qpro/myapplication/TESTGITHUB.java
@@ -0,0 +1,13 @@
+package com.example.qpro.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TESTGITHUB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testgithub);
+    }
+}
